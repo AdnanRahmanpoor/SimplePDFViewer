4c00a1f98830e8ad5d2351102511b0e3a95da457
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2023-12-22 23:26:16 +0400

    created and working on src/main.ts

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow: BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+}
