9fe510296c546b247d1188344b95e3ff424502e1
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2023-12-23 13:37:04 +0400

    main.ts file

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,22 @@ function createWindow() {
   });
 
   mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
